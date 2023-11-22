4392a0367ad3a4eb389bab56327f13829038a582
facundo <facundoladiv99@gmail.com>
2023-11-21 21:06:19 -0300

    fin ejercicios modulo 3

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -1,3 +1,4 @@
+
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
@@ -5,36 +6,69 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x===y) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1.length== str2.length){
+      return true;
+   }  else{
+      return false;
+   }
+   
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   var devolver = false;
+   if (num<90){
+      devolver = true;
+   } 
+   return devolver;
+   }
+
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   var devolver= false;
+   if (num>50){
+      devolver= true;
+   }
+   return devolver;
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   var devolver= false;
+   if (num %2 ==0){
+      devolver=true;
+   }
+   return devolver
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   var devolver= false;
+   if (num %2 != 0){
+      devolver=true;
+   }
+   return devolver;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
